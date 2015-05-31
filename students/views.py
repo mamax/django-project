aa321eeadc57847d37e14154eb7adbf79a9615ca
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-05-31 15:56:49 +0300

    add journal list

--- students/views.py
+++ students/views.py
@@ -63,7 +63,7 @@ def groups_delete(request, gid):
 
 # Views for Journal
 def journal_list(request):
-    return HttpResponse('<h1>Journal List</h1>')
+    return render(request, 'students/journal_list.html', {})
 
 def journal_view(request, gid):
     return HttpResponse('<h1>View Journal %s</h1>' % gid)
